e91871800f33c5ed7d74230a5a7926ec8ad5cb07
Marko Ludlow and monique st laurent <monique@>
2017-04-20 11:32:05 -0700

    add number of nickels, dimes, and quarters for input and push into array.

--- lib/change_machine.rb
+++ lib/change_machine.rb
@@ -1,10 +1,28 @@
 class Fixnum
   define_method(:change_machine) do
     change = (self)
-    coin_hash = {
-      "quarters" => 25
-    }
+    coins_array = []
 
-    number_of_quarters = ((change/25)%25)
+    number_of_quarters = (change/25).floor()
+    coins_array.push(number_of_quarters)
+
+    change_after_quarters = (change%25)
+
+    number_of_dimes = (change_after_quarters/10).floor()
+    coins_array.push(number_of_dimes)
+
+    change_after_dimes = (change_after_quarters%10)
+
+    number_of_nickels = (change_after_dimes/5).floor()
+    coins_array.push(number_of_nickels)
+
+    change_after_nickels = (change_after_dimes%5)
+
+    number_of_pennies = change_after_nickels
+    coins_array.push(number_of_pennies)
+
+
+    coins_array
   end
+
 end
--- spec/change_machine_spec.rb
+++ spec/change_machine_spec.rb
@@ -3,11 +3,16 @@ require('change_machine')
 require('pry')
 
 describe('Fixnum#change_machine') do
-  it('returns number of quarters for number inputted by user') do
-    expect(75.change_machine()).to(eq(3))
+  it('returns number of quarters into an array for number inputted by user') do
+    expect(75.change_machine()).to(eq([3, 0, 0, 0]))
   end
-
-  it('returns number of quarters for number inputted by user') do
-    expect(75.change_machine()).to(eq(3))
+  it('returns number of dimes into an array for number inputted by user') do
+    expect(42.change_machine()).to(eq([1, 1, 1, 2]))
+  end
+  it('returns number of nickels into an array for number inputted by user') do
+    expect(30.change_machine()).to(eq([1, 0, 1, 0]))
+  end
+  it('returns number of pennies into an array for number inputted by user') do
+    expect(42.change_machine()).to(eq([1, 1, 1, 2]))
   end
 end
